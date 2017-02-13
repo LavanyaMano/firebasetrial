762575fad9138a1792fbce0745e495c9f6589066
lava m <lavanymkanna@gmail.com>
2017-02-12 21:47:56 -0500

    Initial Commit

---
+++ Trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trial
+//
+//  Created by lava m on 2/12/17.
+//  Copyright © 2017 Firebase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
